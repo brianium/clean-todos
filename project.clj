e28858badd77d1445425f1718c8266536294ce1f
Brian Scaturro <scaturrob@gmail.com>
2017-07-24 18:41:25 -0400

    Moves core of app to app dir

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject todos "0.1.0-SNAPSHOT"
-  :description "A simple clean architecture example in Clojure"
+(defproject todos/app "0.1.0-SNAPSHOT"
+  :description "A simple clean architecture example in Clojure. This is the main parent project.clj
+                to be referenced by delivery apps"
   
   :url "https://github.com/brianium/clean-todos"
   
@@ -9,5 +10,9 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/core.async "0.3.443"]]
 
-  :profiles {:dev {:source-paths ["src" "test"]
+  :source-paths ["app/src"]
+
+  :test-paths ["app/test"]
+
+  :profiles {:dev {:source-paths ["app/src" "app/test"]
                    :dependencies [[org.clojure/test.check "0.10.0-alpha2"]]}})
